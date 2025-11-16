8118807c37014d4859fe94984014c3629814b9da
Agwaze Great <agwazegreat@gmail.com>
2025-11-17 00:06:24 +0100

    urdf inertia

--- uniteedog_description/launch/display.launch.py
+++ uniteedog_description/launch/display.launch.py
@@ -1,34 +1,16 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
-import os
+from launch.actions import ExecuteProcess
 
 def generate_launch_description():
-
-    pkg_path = get_package_share_directory('uniteedog_description')
-
-    urdf_path = os.path.join(pkg_path, 'urdf', 'unitreeDog.urdf')
-    rviz_config_path = os.path.join(pkg_path, 'rviz', 'unitreeDog.rviz')
-
     return LaunchDescription([
-
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            parameters=[{
-                'robot_description': open(urdf_path).read()
-            }]
+        ExecuteProcess(
+            cmd=['ros2', 'launch', 'ros_ign_gazebo', 'empty_world.launch.py'],
+            output='screen'
         ),
-
-        Node(
-            package='joint_state_publisher',
-            executable='joint_state_publisher'
-        ),
-
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            arguments=['-d', rviz_config_path]
-        ),
-
+        ExecuteProcess(
+            cmd=['ros2', 'run', 'ros_g', 'create',
+                 '-file', '/path/to/unitreeDog.urdf',
+                 '-name', 'unitree_go1'],
+            output='screen'
+        )
     ])
